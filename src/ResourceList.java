8f43c26759c4ca75d47d5287eec170b8abae8989
Dennis Ong <cs.dennisong@gmail.com>
2019-01-28 13:57:26 -0800

    Added ResourceList constructor

--- src/ResourceList.java
+++ src/ResourceList.java
@@ -1,2 +1,13 @@
 public class ResourceList {
+    Resource firstResource;
+    Resource secondResource;
+    Resource thirdResource;
+    Resource fourthResource;
+
+    public ResourceList() {
+        firstResource = new Resource("R1", 1);
+        secondResource = new Resource("R2", 2);
+        thirdResource = new Resource("R3", 3);
+        fourthResource = new Resource("R4", 4);
+    }
 }
