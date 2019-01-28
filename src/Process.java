ae642c61cbe39271cbb62a4c74a9f8b3278d326a
denn9s <cs.dennisong@gmail.com>
2019-01-27 18:04:18 -0800

    Added barebones Process constructor

--- src/Process.java
+++ src/Process.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Process {
+    String processID;
+    ArrayList<Integer> otherResources;
+    int priority;
+
+    public Process(String pid, Process parent, int priority, int index) {
+        this.processID = pid;
+        this.otherResources = new ArrayList<Integer>();
+        this.priority = priority;
+    }
 }
