284bdba7e1d3b552af6b485fc1d2998b361b4bba
denn9s <cs.dennisong@gmail.com>
2019-01-27 19:06:43 -0800

    Added PriorityList constructor with three levels

--- src/PriorityList.java
+++ src/PriorityList.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
 public class PriorityList {
-    // 0 = init, 1 =
+    // 0 = init, 1 = user, 2 = system
+    Process initProcess;
+    ArrayList<Process> userProcesses;
+    ArrayList<Process> systemProcesses;
+
+    public PriorityList() {
+        initProcess = new Process("init", null, 0, -1);
+        userProcesses = new ArrayList<Process>();
+        systemProcesses = new ArrayList<Process>();
+    }
 }
