0e4f5c650fd5a793c82084468c068e465911db2c
Dennis Ong <cs.dennisong@gmail.com>
2019-01-28 13:08:16 -0800

    Updated to include creation tree parent/children in constructor

--- src/Process.java
+++ src/Process.java
@@ -4,11 +4,19 @@ public class Process {
     String processID;
     ArrayList<Integer> otherResources;
     int priority;
+    Process creationTreeParent;
+    ArrayList<Process> creationTreeChildren;
 
     public Process(String pid, Process parent, int priority, int index) {
         this.processID = pid;
         this.otherResources = new ArrayList<Integer>();
         this.priority = priority;
+        if (parent == null) {
+            this.creationTreeParent = this;
+        } else {
+            this.creationTreeParent = parent;
+        }
+        this.creationTreeChildren = new ArrayList<Process>();
     }
 
     public int getPriority() {
